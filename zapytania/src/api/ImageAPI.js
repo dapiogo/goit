fd4ac16aafbff5da2cbbde0aaad99f76a0f612aa
dawidgola <92149687+dawidgola@users.noreply.github.com>
2023-07-13 23:59:04 +0200

    update

--- zapytania/src/api/ImageAPI.js
+++ zapytania/src/api/ImageAPI.js
@@ -1,7 +1,9 @@
+const API_KEY = "38240917-a3d7c9351f3e7e3fe8e3a9bdf"
+
 export const fetchImages = async (inputSearch) => {
     try {
       const response = await fetch(
-        `https://pixabay.com/api/?key=38240917-a3d7c9351f3e7e3fe8e3a9bdf&q=${inputSearch}&image_type=photo&pretty=true`
+        `https://pixabay.com/api/?key=${API_KEY}&q=${inputSearch}&image_type=photo&pretty=true`
       );
 
       if (!response.ok) {
